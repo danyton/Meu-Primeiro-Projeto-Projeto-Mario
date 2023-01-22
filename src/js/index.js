3bb867d07c0bc5016cf45354f26b377614def11d
Heverton Freire Barbosa <108628363+danyton@users.noreply.github.com>
2023-01-22 10:36:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function altrenarModal(){
+    modal.classeList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", ()=>{
+    atrenarModal(); 
+    video.setAttribute("src", linkDoVideo);
+}); 
+
+botaoFecharModal.addEventListener("click",() =>{
+    atrenarModal();
+video.setAttribute("src","");
+})
+
+
+ 
+    
+
+
+
+
